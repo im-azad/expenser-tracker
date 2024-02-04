1feaca11dbb94b9e8fcbc25e520aa89e97daadba
im-azad <me.azad99@gmail.com>
2024-02-04 22:38:47 +0600

    form handel add

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,51 +1,69 @@
+import { useState } from "react";
+
 export default function Form() {
-    return (
-        <div className="form">
-            <h3>Add new transaction</h3>
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [amount, setAmount] = useState("");
 
-            <div className="form-group">
-                <label htmlFor="transaction_name">Name</label>
-                <input
-                    type="text"
-                    name="transaction_name"
-                    placeholder="My Salary"
-                />
-            </div>
+  const handleCreate = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="form">
+      <h3>Add new transaction</h3>
 
-            <div className="form-group radio">
-                <label htmlFor="transaction_type">Type</label>
-                <div className="radio_group">
-                    <input
-                        type="radio"
-                        value="income"
-                        name="transaction_type"
-                        checked
-                    />
-                    <label htmlFor="transaction_type">Income</label>
-                </div>
-                <div className="radio_group">
-                    <input
-                        type="radio"
-                        value="expense"
-                        name="transaction_type"
-                        placeholder="Expense"
-                    />
-                    <label htmlFor="transaction_type">Expense</label>
-                </div>
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="transaction_amount">Amount</label>
-                <input
-                    type="number"
-                    placeholder="300"
-                    name="transaction_amount"
-                />
-            </div>
-
-            <button className="btn">Add Transaction</button>
-
-            <button className="btn cancel_edit">Cancel Edit</button>
+      <form onSubmit={handleCreate}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            required
+            type="text"
+            name="name"
+            placeholder="enter title"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-    );
+        <div className="form-group radio">
+          <label>Type</label>
+          <div className="radio_group">
+            <input
+              type="radio"
+              required
+              value="income"
+              name="type"
+              checked={type === "income"}
+              onChange={(e) => setType(e.target.value)}
+            />
+            <label>Income</label>
+          </div>
+          <div className="radio_group">
+            <input
+              type="radio"
+              value="expense"
+              required
+              name="type"
+              placeholder="Expense"
+              checked={type === "expense"}
+              onChange={(e) => setType(e.target.value)}
+            />
+            <label>Expense</label>
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Amount</label>
+          <input
+            type="number"
+            required
+            name="amount"
+            placeholder="enter amount"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <button className="btn" type="submit">
+          Add Transaction
+        </button>
+      </form>
+      <button className="btn cancel_edit">Cancel Edit</button>
+    </div>
+  );
 }
