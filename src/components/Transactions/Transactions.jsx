3ee60dfad59176d8a4f072bdfb1d68c3cb2c4ff6
im-azad <me.azad99@gmail.com>
2024-02-05 23:00:59 +0600

    initail transaction showing

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,20 +1,26 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTransactions } from "../../features/transaction/transactionSlice";
 import Transaction from "./Transaction";
 export default function Transactions() {
+  const dispatch = useDispatch();
   const { isLoading, isError, transactions } = useSelector(
     (state) => state.transaction
   );
 
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
   let content = null;
   if (isLoading) {
     content = <p>Loading...</p>;
   }
   if (!isLoading && isError) {
-    content = <p className="error">There was and error {transactions.error}</p>;
+           content = <p className="error">There was and error {transactions.error}</p>;
   }
 
   if (!isLoading && !isError && transactions.length === 0) {
-    content = <p>No transactions found</p>;
+        content =      <p>No transactions found</p>;
   }
   if (!isLoading && !isError && transactions.length > 0) {
     content = transactions.map((transaction) => (
@@ -23,12 +29,12 @@ export default function Transactions() {
   }
   return (
     <>
-      <p className="second_heading " style={ {color: "blue"}}>Your Transactions:</p>
+      <p className="second_heading " style={{ color: "blue" }}>
+        Your Transactions:
+      </p>
 
       <div className="conatiner_of_list_of_transactions">
-        <ul>
-         {content}
-        </ul>
+        <ul>{content}</ul>
       </div>
     </>
   );
