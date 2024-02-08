f8f0f9fe0119df27416374b7dc7594d9360c30bb
im-azad <me.azad99@gmail.com>
2024-02-08 22:45:46 +0600

    calculate blance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,11 +1,27 @@
+import { useSelector } from "react-redux";
+
 export default function Balance() {
+    const { transactions } = useSelector((state) => state.transaction);
+
+    const calculateIncome = () => {
+        let income = 0;
+        transactions.forEach((transaction) => {
+            if (transaction.type === "income") {
+                income += transaction.amount;
+            } else {
+                income -= transaction.amount;
+            }
+        });
+        return income;
+    };
+
     return (
-        <div className="top_card">
-            <p>Your Current Balance</p>
-            <h3>
-                <span>৳</span>
-                <span>10500</span>
-            </h3>
-        </div>
-    );
+		<div className="top_card">
+			<p>Your Current Balance</p>
+			<h3>
+				<span>৳</span>{" "}
+				{transactions.length > 0 ? <span>{calculateIncome()}</span> : 0}
+			</h3>
+		</div>
+	);
 }
